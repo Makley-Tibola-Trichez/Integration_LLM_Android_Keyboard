db3cd47f17ee7e3ae402ecd5e9320b166e62e82e
Makley <tibolamakley1@gmail.com>
2023-07-05 19:38:35 -0300

    feat: implements basic keyboard

---
+++ app/src/main/java/com/example/keyboard_learning/MyInputMethod.kt
@@ -0,0 +1,74 @@
+package com.example.keyboard_learning
+
+import android.annotation.SuppressLint
+import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener
+import android.media.AudioManager
+import android.view.KeyEvent
+import android.view.View
+
+
+class MyInputMethod : InputMethodService(), OnKeyboardActionListener {
+    private var keyboardView: KeyboardView? = null
+    private var keyboard: Keyboard? = null
+    var isCaps = false
+    override fun onPress(primaryCode: Int) {}
+    override fun onRelease(primaryCode: Int) {}
+
+    override fun onCreateInputView(): View {
+        keyboardView = layoutInflater.inflate(R.layout.keyboard, null) as KeyboardView?
+        keyboard = Keyboard(this, R.xml.qwerty)
+        keyboardView!!.keyboard = keyboard
+        keyboardView!!.setOnKeyboardActionListener(this)
+        return keyboardView!!
+    }
+
+    override fun onKey(primaryCode: Int, keyCodes: IntArray) {
+        val inputConnection = currentInputConnection
+        playClick(primaryCode)
+        when (primaryCode) {
+            Keyboard.KEYCODE_DELETE -> inputConnection.deleteSurroundingText(1, 0)
+            Keyboard.KEYCODE_SHIFT -> {
+                isCaps = !isCaps
+                keyboard!!.isShifted = isCaps
+                keyboardView!!.invalidateAllKeys()
+            }
+
+            Keyboard.KEYCODE_DONE -> inputConnection.sendKeyEvent(
+                KeyEvent(
+                    KeyEvent.ACTION_DOWN,
+                    KeyEvent.KEYCODE_ENTER
+                )
+            )
+
+            else -> {
+                var code = primaryCode.toChar()
+                if (Character.isLetter(code) && isCaps) {
+                    code = code.uppercaseChar()
+                }
+                inputConnection.commitText(code.toString(), 1)
+            }
+        }
+    }
+
+    private fun playClick(i: Int) {
+        val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        when (i) {
+            32 -> audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR)
+            Keyboard.KEYCODE_DONE, 10 -> audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN)
+            Keyboard.KEYCODE_DELETE -> audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE)
+            else -> audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD)
+        }
+    }
+
+    override fun onText(text: CharSequence) {}
+    override fun swipeLeft() {}
+    override fun swipeRight() {}
+    override fun swipeDown() {}
+    override fun swipeUp() {}
+}
+
+
+
