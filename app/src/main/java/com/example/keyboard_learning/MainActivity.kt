556baab7cfb1acd61e65514e93912ca42e97c11b
Makley <tibolamakley1@gmail.com>
2023-08-12 13:49:31 -0300

    refactor: remove unused files, remove Composables, and move MyInputMethod.kt to service package

--- app/src/main/java/com/example/keyboard_learning/MainActivity.kt
+++ app/src/main/java/com/example/keyboard_learning/MainActivity.kt
@@ -2,36 +2,12 @@ package com.example.keyboard_learning
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.keyboard_learning.ui.theme.KeyboardlearningTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContent(R.layout.screen);
         setContentView(R.layout.screen)
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KeyboardlearningTheme {
-        Greeting("Android")
     }
 }
